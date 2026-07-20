72ccd2275f8a7895e6d49542f2ff9b367242602d
pegasus120mouan <coulibaly.fisher@yahoo.com>
2026-07-20 20:35:21 +0000

    updated20072026.

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -300,17 +300,17 @@
                 <div class="text-truncate" data-i18n="User interface">Gestion financière</div>
               </a>
               <ul class="menu-sub">
-              <li class="menu-item {{ request()->routeIs('effectuer_paiement.*') ? 'active' : '' }}">
+            <!--  <li class="menu-item {{ request()->routeIs('effectuer_paiement.*') ? 'active' : '' }}">
                   <a href="{{ route('effectuer_paiement.index') }}" class="menu-link">
                     <div class="text-truncate" data-i18n="Accordion">Effectuer un paiement</div>
                   </a>
-                </li>
+                </li>-->
 
-                <li class="menu-item {{ request()->routeIs('solde_chef_equipe.*') ? 'active' : '' }}">
+             <!--   <li class="menu-item {{ request()->routeIs('solde_chef_equipe.*') ? 'active' : '' }}">
                   <a href="{{ route('solde_chef_equipe.index') }}" class="menu-link">
                     <div class="text-truncate" data-i18n="Accordion">Token chef d'équipe</div>
                   </a>
-                </li>
+                </li>-->
               <li class="menu-item">
                   <a href="{{ route('gestionfinanciere.montant_agent') }}" class="menu-link">
                     <div class="text-truncate" data-i18n="Accordion">Montant Pisteur</div>
@@ -1102,6 +1102,101 @@
                 <li class="menu-item">
                   <a href="{{ route('avances_transporteur.index') }}" class="menu-link">
                     <div class="text-truncate" data-i18n="Accordion">Avances transporteurs</div>
+                  </a>
+                </li>
+               <!--  <li class="menu-item">
+                  <a href="ui-buttons.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Buttons">Buttons</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-carousel.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Carousel">Carousel</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-collapse.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Collapse">Collapse</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-dropdowns.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Dropdowns">Dropdowns</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-footer.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Footer">Footer</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-list-groups.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="List Groups">List groups</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-modals.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Modals">Modals</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-navbar.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Navbar">Navbar</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-offcanvas.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Offcanvas">Offcanvas</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-pagination-breadcrumbs.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Pagination & Breadcrumbs">Pagination &amp; Breadcrumbs</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-progress.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Progress">Progress</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-spinners.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Spinners">Spinners</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-tabs-pills.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Tabs & Pills">Tabs &amp; Pills</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-toasts.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Toasts">Toasts</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-tooltips-popovers.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Tooltips & Popovers">Tooltips &amp; Popovers</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="ui-typography.html" class="menu-link">
+                    <div class="text-truncate" data-i18n="Typography">Typography</div>
+                  </a>
+                </li>-->
+              </ul>
+            </li>
+
+
+            <li class="menu-item">
+              <a href="javascript:void(0)" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-box"></i>
+                <div class="text-truncate" data-i18n="User interface">Paramètres Généraux</div>
+              </a>
+              <ul class="menu-sub">
+              <li class="menu-item {{ request()->routeIs('solde_chef_equipe.*') ? 'active' : '' }}">
+                  <a href="{{ route('solde_chef_equipe.index') }}" class="menu-link">
+                    <div class="text-truncate" data-i18n="Accordion">Token chef d'équipe</div>
                   </a>
                 </li>
                <!--  <li class="menu-item">
